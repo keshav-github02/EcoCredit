64bf0aa6039c347ade40e493f1bd4c40139e8a4c
keshav-github02 <keshav.2022ug1022@iiitranchi.ac.in>
2025-07-22 11:36:14 +0530

    image page

--- lib/pages/upload_item.dart
+++ lib/pages/upload_item.dart
@@ -16,24 +16,50 @@ class _UploadItemState extends State<UploadItem> {
         margin: EdgeInsets.only(top: 40.0),
         child: Column(
           children: [
-            Row(
+            Padding(
+              padding: const EdgeInsets.only(left: 20.0),
+              child: Row(
 
-              children: [
-                
-                Material(
-                  elevation: 3.0,
-                  borderRadius: BorderRadius.circular(60),
-                  child: Container(
-                    padding: EdgeInsets.all(6),
-                    decoration: BoxDecoration(
-                      color: Colors.black,
-                      borderRadius: BorderRadius.circular(60),
+                children: [
+
+                  Material(
+                    elevation: 3.0,
+                    borderRadius: BorderRadius.circular(60),
+                    child: Container(
+                      padding: EdgeInsets.all(6),
+                      decoration: BoxDecoration(
+                        color: Colors.black,
+                        borderRadius: BorderRadius.circular(60),
+                      ),
+                      child: Icon(Icons.arrow_back_ios_new_rounded,color: Colors.white,size: 30.0,),
                     ),
-                    child: Icon(Icons.arrow_back_ios_new_rounded,color: Colors.white,),
                   ),
-                ),
-                Text("Upload Item",style: AppWidget.headlineTextstyle(22.0),),
-              ],
+                  SizedBox(
+                    width: MediaQuery.of(context).size.width/5.5,
+                  ),
+                  Text("Upload Item",style: AppWidget.headlineTextstyle(25.0),),
+                ],
+              ),
+            ),
+            Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.only(topLeft: Radius.circular(30),topRight: Radius.circular(30)),
+                color: Color(0xFFececf8),
+
+              ),
+              child: Column(
+                children: [
+                  Container(
+
+                   height: 150,
+                    width: 150,
+                    decoration: BoxDecoration(
+
+                      color: Colors.white,
+                    ),
+                  ),
+                ],
+              ),
             ),
 
           ],
