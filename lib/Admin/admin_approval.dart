2beaffeeb4aa8be06381fe7f530974c32c75b60f
keshav-github02 <keshav.2022ug1022@iiitranchi.ac.in>
2025-07-27 16:16:33 +0530

    making admin approval

--- lib/Admin/admin_approval.dart
+++ lib/Admin/admin_approval.dart
@@ -1,1 +1,67 @@
 import 'package:flutter/material.dart';
+
+import '../services/widget_support.dart';
+
+class AdminApproval extends StatefulWidget {
+  const AdminApproval({super.key});
+
+  @override
+  State<AdminApproval> createState() => _AdminApprovalState();
+}
+
+class _AdminApprovalState extends State<AdminApproval> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        margin: EdgeInsets.only(top: 40.0),
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(left: 20.0),
+              child: Row(
+                children: [
+                  Material(
+                    elevation: 3.0,
+                    borderRadius: BorderRadius.circular(60),
+                    child: Container(
+                      padding: EdgeInsets.all(6),
+                      decoration: BoxDecoration(
+                        color: Colors.black,
+                        borderRadius: BorderRadius.circular(60),
+                      ),
+                      child: Icon(
+                        Icons.arrow_back_ios_new_rounded,
+                        color: Colors.white,
+                        size: 30.0,
+                      ),
+                    ),
+                  ),
+                  SizedBox(width: MediaQuery.of(context).size.width / 7),
+                  Text("Admin Approval", style: AppWidget.headlineTextstyle(25.0)),
+                ],
+              ),
+            ),
+            SizedBox(height: 20.0),
+            Expanded(
+              child: Container(
+                width: MediaQuery.of(context).size.width,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(30),
+                    topRight: Radius.circular(30),
+                  ),
+                  color: Color(0xFFececf8),
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
