5bf221fb2b26d16d602437c0cbcfef2a0b9f26ca
keshav-github02 <keshav.2022ug1022@iiitranchi.ac.in>
2025-08-07 23:52:42 +0530

    making admin approval

--- lib/Admin/admin_approval.dart
+++ lib/Admin/admin_approval.dart
@@ -55,7 +55,7 @@ class _AdminApprovalState extends State<AdminApproval> {
                 ),
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [],
+
                 ),
               ),
             ),
